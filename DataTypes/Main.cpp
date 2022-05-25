72104502c67c799870dc248c0491ae1cdca5345d
Wilson Huang <window012@hotmail.com>
2022-05-26 07:36:24 +0800

    Add project files.

---
+++ DataTypes/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "8 bits = 1 byte" << std::endl;
+	std::cout << "32 bits = 4 bytes" << std::endl;
+	std::cout << "64 bits = 8 bytes" << std::endl;
+	std::cout << std::endl;
+	std::cout << "int: " << sizeof(int) << " bytes" << std::endl;
+	std::cout << "char: " << sizeof(char) << " byte" << std::endl;
+	std::cout << "float: " << sizeof(float) << " bytes" << std::endl;
+	std::cout << "double: " << sizeof(double) << " bytes" << std::endl;
+	std::cout << "short: " << sizeof(short) << " bytes" << std::endl;
+	std::cout << "long: " << sizeof(long) << " bytes" << std::endl;
+	std::cout << "long long: " << sizeof(long long) << " bytes" << std::endl;
+	std::cout << "size_t: " << sizeof(size_t) << " bytes" << std::endl;
+	std::cout << "unsigned int: " << sizeof(unsigned int) << " bytes" << std::endl;
+	int* p;
+	void* v;
+	std::cout << "pointer of int: " << sizeof(p) << " bytes" << std::endl;
+	std::cout << "pointer of void: " << sizeof(v) << " bytes" << std::endl;
+
+	return 0;
+}
